d2a80d7a359ab5f307b7cb3c63e247d988d5e980
Wiktor Mazur <wiktormazur1@gmail.com>
2020-05-28 18:01:33 +0200

    packetprinter bin

---
+++ src/packetprinter.rs
@@ -0,0 +1,18 @@
+
+use std::sync::mpsc::{channel, Receiver, Sender};
+use packet_sniffer::UdpPacket;
+
+fn main() {
+    if let Ok(interfaces) = packet_sniffer::network_interfaces() {
+        let (tx, rx): (Sender<UdpPacket>, Receiver<UdpPacket>) = channel();
+        packet_sniffer::receive(interfaces, tx);
+
+
+        loop {
+            if let Ok(packet) = rx.recv() {
+                println!("Received packet: {:?}", &packet);
+            }
+        }
+    }
+
+}
