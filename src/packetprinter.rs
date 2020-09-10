38d6afe22f8d25d7d3b7e2c9269966918a72f928
Wiktor Mazur <wiktormazur1@gmail.com>
2020-09-10 23:47:13 +0200

    Introduced debugging utility

--- src/packetprinter.rs
+++ src/packetprinter.rs
@@ -1,21 +1,62 @@
-
+use clap::{App, Arg};
 use std::sync::mpsc::{channel, Receiver, Sender};
+
+mod photon_decode;
 
 mod packet_sniffer;
 
 use packet_sniffer::UdpPacket;
 
 fn main() {
+    let matches = App::new("Testing utility")
+        .about("Debug and develop new features")
+        .arg(
+            Arg::with_name("MODE")
+                .help("What mode to run the program in")
+                .index(1)
+                .possible_values(&["raw", "decoding"])
+                .default_value("raw")
+                .required(true),
+        )
+        .arg(
+            Arg::with_name("port")
+                .short("p")
+                .long("port")
+                .takes_value(true)
+                .help("UDP packets port")
+        )
+        .get_matches();
+
     if let Ok(interfaces) = packet_sniffer::network_interfaces() {
         let (tx, rx): (Sender<UdpPacket>, Receiver<UdpPacket>) = channel();
         packet_sniffer::receive(interfaces, tx);
 
-
         loop {
             if let Ok(packet) = rx.recv() {
-                println!("Received packet: {:?}", &packet);
+
+                if let Some(port) = matches.value_of("port") {
+                    let port_number = port.parse::<u16>().unwrap();
+
+                    if packet.destination_port != port_number && packet.source_port != port_number {
+                        continue;
+                    }
+                };
+
+                match matches.value_of("MODE").unwrap() {
+                    "raw" => {
+                        println!("{:?}", packet);
+                    }
+                    "decoding" => {
+                        let mut photon = photon_decode::Photon::new();
+
+                        photon
+                            .try_decode(&packet.payload)
+                            .into_iter()
+                            .for_each(|p| println!("{:?}", p));
+                    }
+                    _ => unreachable!(),
+                }
             }
         }
     }
-
 }
